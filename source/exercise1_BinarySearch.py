7ec792d08841a31b8dcd7c8169b418c046d16f96
YuvalRi <yuvalritsker@gmail.com>
2022-11-04 13:01:13 +0200

    ex1 works

--- source/exercise1_BinarySearch.py
+++ source/exercise1_BinarySearch.py
@@ -13,11 +13,11 @@ def unimodal_index(A: list, n: int):
     min = 0
     max = n-1
     while (found == False and min <= max):
-        mid = math.floor((A[0]+A[n-1])/2)
+        mid = math.floor((min+max)/2)
         if(A[mid] > A[mid+1] and A[mid] > A[mid-1]):
             found = True
         else:
-            if (A[mid] < A[mid+1]):
+            if (A[mid] > A[mid+1]):
                 max = mid - 1
             else:
                 min = mid + 1
@@ -27,5 +27,8 @@ def unimodal_index(A: list, n: int):
 
 if __name__ == "__main__":
     print(unimodal_index(A=[1,3,7,10,8,5,3], n=7))
-    print("2")
+    print(unimodal_index(A=[1,10,9,8,7,6,5], n=7))
+    print(unimodal_index(A=[1,2,9,8,7,6,5], n=7))
+
+
 
