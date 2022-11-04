093eda931d088b3051e5586d28b7330e54b43f59
YuvalRi <yuvalritsker@gmail.com>
2022-11-04 13:04:46 +0200

    order main code line

--- source/exercise1_BinarySearch.py
+++ source/exercise1_BinarySearch.py
@@ -26,9 +26,10 @@ def unimodal_index(A: list, n: int):
 
 
 if __name__ == "__main__":
-    print(unimodal_index(A=[1,3,7,10,8,5,3], n=7))
-    print(unimodal_index(A=[1,10,9,8,7,6,5], n=7))
-    print(unimodal_index(A=[1,2,9,8,7,6,5], n=7))
+    # example
+    unimodal_index(A=[1,3,7,10,8,5,3], n=7)
+    unimodal_index(A=[1,10,9,8,7,6,5], n=7)
+    unimodal_index(A=[1,2,9,8,7,6,5], n=7)
 
 
 
