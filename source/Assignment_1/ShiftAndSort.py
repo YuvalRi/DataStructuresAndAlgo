6fac7cca17df11bbd59c65146c38024e84b715f2
YuvalRi <yuvalritsker@gmail.com>
2022-11-06 23:38:58 +0200

    shift and sort works Co-authored-by: ofekshmu <ofekshmu@users.noreply.github.com>

--- source/Assignment_1/ShiftAndSort.py
+++ source/Assignment_1/ShiftAndSort.py
@@ -1,7 +1,24 @@
-
+import math
 
 def shift_and_sort(A1: list, n: int, val: int):
     found = False
     start = 0
     end = n-1
-    
+    while found == False and start <= end:
+        mid = math.floor((start+end)/2)
+        if A1[mid] == val:
+            found = True
+        elif val < A1[start]:
+            start = mid + 1
+            if val == A1[end]:
+                return end
+        elif val >= A1[start]:
+            end = mid - 1
+            if val == A1[end]:
+                return end
+    if found:
+        return mid
+    return -1        
+
+            
+
---
+++ source/Assignment_1/shift_tests.py
@@ -0,0 +1,12 @@
+from ShiftAndSort import shift_and_sort
+
+if __name__ == "__main__":
+    lst1 = [9,14,23,1,5,6,8]
+    print(shift_and_sort(A1=lst1, n=len(lst1),val = lst1[0]))
+    print(shift_and_sort(A1=lst1, n=len(lst1),val = lst1[1]))
+    print(shift_and_sort(A1=lst1, n=len(lst1),val = lst1[2]))
+    print(shift_and_sort(A1=lst1, n=len(lst1),val = lst1[3]))
+    print(shift_and_sort(A1=lst1, n=len(lst1),val = lst1[4]))
+    print(shift_and_sort(A1=lst1, n=len(lst1),val = lst1[5]))
+    print(shift_and_sort(A1=lst1, n=len(lst1),val = lst1[6]))
+
