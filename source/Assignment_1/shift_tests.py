0694924f52f1456d09fa28d8850879933b67f68e
YuvalRi <yuvalritsker@gmail.com>
2022-11-07 12:52:15 +0200

    binary search algo

--- source/Assignment_1/shift_tests.py
+++ source/Assignment_1/shift_tests.py
@@ -9,4 +9,40 @@ if __name__ == "__main__":
     print(shift_and_sort(A1=lst1, n=len(lst1),val = lst1[4]))
     print(shift_and_sort(A1=lst1, n=len(lst1),val = lst1[5]))
     print(shift_and_sort(A1=lst1, n=len(lst1),val = lst1[6]))
+    lst2 = [4,5,6,1,2,3]
+    print(shift_and_sort(A1=lst2, n=len(lst2),val = lst2[0]))
+    print(shift_and_sort(A1=lst2, n=len(lst2),val = lst2[1]))
+    print(shift_and_sort(A1=lst2, n=len(lst2),val = lst2[2]))
+    print(shift_and_sort(A1=lst2, n=len(lst2),val = lst2[3]))
+    print(shift_and_sort(A1=lst2, n=len(lst2),val = lst2[4]))
+    print(shift_and_sort(A1=lst2, n=len(lst2),val = lst2[5]))
+    lst3 = [6,1,2,3,4,5]
+    print(shift_and_sort(A1=lst3, n=len(lst3),val = lst3[0]))
+    print(shift_and_sort(A1=lst3, n=len(lst3),val = lst3[1]))
+    print(shift_and_sort(A1=lst3, n=len(lst3),val = lst3[2]))
+    print(shift_and_sort(A1=lst3, n=len(lst3),val = lst3[3]))
+    print(shift_and_sort(A1=lst3, n=len(lst3),val = lst3[4]))
+    print(shift_and_sort(A1=lst3, n=len(lst3),val = lst3[5]))
+    lst3 = [5,6,1,2,3,4]
+    print(shift_and_sort(A1=lst3, n=len(lst3),val = lst3[0]))
+    print(shift_and_sort(A1=lst3, n=len(lst3),val = lst3[1]))
+    print(shift_and_sort(A1=lst3, n=len(lst3),val = lst3[2]))
+    print(shift_and_sort(A1=lst3, n=len(lst3),val = lst3[3]))
+    print(shift_and_sort(A1=lst3, n=len(lst3),val = lst3[4]))
+    print(shift_and_sort(A1=lst3, n=len(lst3),val = lst3[5]))
+    lst3 = [3,4,5,6,1,2]
+    print(shift_and_sort(A1=lst3, n=len(lst3),val = lst3[0]))
+    print(shift_and_sort(A1=lst3, n=len(lst3),val = lst3[1]))
+    print(shift_and_sort(A1=lst3, n=len(lst3),val = lst3[2]))
+    print(shift_and_sort(A1=lst3, n=len(lst3),val = lst3[3]))
+    print(shift_and_sort(A1=lst3, n=len(lst3),val = lst3[4]))
+    print(shift_and_sort(A1=lst3, n=len(lst3),val = lst3[5]))
+    lst3 = [2,3,4,5,6,1]
+    print(shift_and_sort(A1=lst3, n=len(lst3),val = lst3[0]))
+    print(shift_and_sort(A1=lst3, n=len(lst3),val = lst3[1]))
+    print(shift_and_sort(A1=lst3, n=len(lst3),val = lst3[2]))
+    print(shift_and_sort(A1=lst3, n=len(lst3),val = lst3[3]))
+    print(shift_and_sort(A1=lst3, n=len(lst3),val = lst3[4]))
+    print(shift_and_sort(A1=lst3, n=len(lst3),val = lst3[5]))
+
 
